a271fe78ce524c3410f4667cdf27f37de5835ddb
balez4464 <balez4464@wrdsb.ca>
2020-10-13 12:54:38 +0000

    example for methods

--- Main.java
+++ Main.java
@@ -1,15 +1,60 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Example program for methods
+ * @author Zachary Balean
  */
 public class Main {
+
+  //create a method to say hello
+  public static void sayHello(){
+    //prints hello world to the screen
+    System.out.println("Hello World!");
+  }
+
+  //prints hello to a person
+  public static void sayHello(String name){
+    System.out.println("Hello " + name);
+  }
+
+  //prints hello world a number of times
+  public static void sayHello(int numberOfTimes){
+    for(int i = 0; i < numberOfTimes; i++){
+      System.out.println("Hello World!");
+    }
+  }
+
+
+
+
+  //A method to calculate the area of a rectangle give the width and the length
+  public static double areaOfRectangle(double width, double length){
+    double area = width * length;
+    return area;
+  }
+
+
+
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+
+    //without a scanner
+    double answer = areaOfRectangle(2.0, 5.0);
+    System.out.println(answer);
+
+    //using the method with a scanner
+    System.out.println("Please enter a width");
+    double width = input.nextDouble();
+    System.out.println("Please enter a length");
+    double length = input.nextDouble();
+    double area = areaOfRectangle(width, length);
+    System.out.println(area);
+
+    //print hellow world
+    sayHello();
   }
 }
